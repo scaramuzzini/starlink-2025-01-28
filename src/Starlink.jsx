ec1351973e497c542b52d1a69610ec3fd9fb19a0
Mario Torres <scaramuzzini@gmail.com>
2025-01-28 09:53:36 -0500

    axios and API added

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -1,5 +1,24 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function Starlink() {
+    useEffect(() => {
+        const fetchStarlink = async () => {
+            const response = await axios.post('https://api.spacexdata.com/v4/starlink/query'
+                , {
+                    "query": {},
+                    "options": {
+                        limit: 10
+                    }
+                }
+            );
+            console.log(response.data.docs);
+        }
+
+        fetchStarlink();
+    });
+
     return <h1>Lista de satélites Starlink</h1>
 }
 
-export default Starlink;
+export default Starlink;    
